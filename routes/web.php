70a9d52d059a9cfe582a5eb482d698b11ea9f4dd
jhenspapirey <narutoygoku6@gmail.com>
2022-12-15 15:08:54 -0500

    inicio de sesion

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,15 @@ use App\Http\Controllers\profesorController;
 */
 //Route::get('/', [asignaturaController::class, 'index']);
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
-Route::resource('/asignaturas', asignaturaController::class)->names('asignatura');
-Route::resource('/profesores', profesorController::class)->names('profesor');
-Route::resource('/alumnos', alumnoController::class)->names('alumno');
-Route::resource('/inscripciones', inscripcionController::class)->names('inscripcion');
+Route::resource('/asignaturas', asignaturaController::class)->names('asignatura')->middleware('auth');
+Route::resource('/profesores', profesorController::class)->names('profesor')->middleware('auth');
+Route::resource('/alumnos', alumnoController::class)->names('alumno')->middleware('auth');
+Route::resource('/inscripciones', inscripcionController::class)->names('inscripcion')->middleware('auth');
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware'=>'auth'],function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
